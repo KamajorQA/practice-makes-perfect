845c7f13e724137f310c0b93b03101a48c7412b7
KamajorQA <KamajorQA@gmail.com>
2023-02-16 23:11:13 +0300

    12 _ map-array-of-objects

---
+++ React_studyflow/12 _ map-array-of-objects/src/App.js
@@ -0,0 +1,10 @@
+import Persons from './components/Persons';
+import './App.css';
+
+function App() {
+  return <div className="App">
+	<Persons />
+</div>;
+}
+
+export default App;
---
+++ React_studyflow/12 _ map-array-of-objects/src/components/Person.js
@@ -0,0 +1,17 @@
+import './person.css';
+
+function Person(props) {
+  const { firstName, lastName, email, img } = props;
+  console.log(props);
+  return (
+    <div className="card">
+      <img src={img} alt="" />
+      <h3>
+        {firstName} {lastName}
+      </h3>
+      <h4>{email}</h4>
+    </div>
+  );
+}
+
+export default Person;
