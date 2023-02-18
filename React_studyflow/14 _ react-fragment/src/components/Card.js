a974a8ab3799f4806d666594d0e976c3f4d48f33
KamajorQA <KamajorQA@gmail.com>
2023-02-18 23:47:00 +0300

    14 _ react-fragment

---
+++ React_studyflow/14 _ react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Kamajor</h1>
+      <h3>Lorem ipsum</h3>
+      <button>Just a button</button>
+    </React.Fragment>
+  );
+};
+
+export default Card;
