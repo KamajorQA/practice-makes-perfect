05f9b195d9f8359f0a5a95ea3e40d867859acc4d
KamajorQA <KamajorQA@gmail.com>
2023-08-10 23:59:11 +0300

    Подключен и протестирован пакет chalk. Добавлен тестовый модуль dataClosures.js.

--- Node.js/Basic/index.js
+++ Node.js/Basic/index.js
@@ -1,1 +1,20 @@
-console.log('Hello, Node.js!');
+const chalk = require('chalk');
+
+const heisenberg = require('./dataClosures');
+
+const ifrit = require('./dataClosures');
+
+console.log(chalk.blue('Hello, Node.js!'));
+
+heisenberg.showScore();
+
+ifrit.increment();
+ifrit.increment();
+ifrit.showScore();
+
+heisenberg.decrement();
+heisenberg.sayMyName();
+heisenberg.showScore();
+
+ifrit.decrement();
+console.log(`${ifrit.sayMyName()}'s score now is: ${ifrit.showScore()}`);
