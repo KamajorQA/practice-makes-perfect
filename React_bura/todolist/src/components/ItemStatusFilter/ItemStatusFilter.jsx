834143fad004d0f94603d3d6059675972bed0b6a
Kamajor <kamajorqa@gmail.com>
2023-09-08 23:37:33 +0300

    Функциональные компоненты, использующие внутреннее состояние переписаны на классовые компоненты

--- React_bura/todolist/src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ React_bura/todolist/src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -1,19 +1,23 @@
+import React from 'react';
 import './itemStatusFilter.css';
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info active-button">
-        All
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
-};
+class ItemStatusFilter extends React.Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-info active-button">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
 
 export default ItemStatusFilter;
--- React_bura/todolist/src/components/TodoListItem/TodoListItem.jsx
+++ React_bura/todolist/src/components/TodoListItem/TodoListItem.jsx
@@ -1,29 +1,34 @@
+import React from 'react';
 import { ReactComponent as Lightning } from '../../assets/icons/Lightning.svg';
 import { ReactComponent as Trash } from '../../assets/icons/Trash.svg';
 import './todoListItem.css';
 
-function TodoListItem({ label, important = false }) {
-  const itemStyle = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  };
+class TodoListItem extends React.Component {
+  render() {
+    const { label, important = false } = this.props;
 
-  return (
-    <span className="todo-list-item ">
-      <span className="todo-list-item-label" style={itemStyle}>
-        {label}
+    const itemStyle = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    };
+
+    return (
+      <span className="todo-list-item ">
+        <span className="todo-list-item-label" style={itemStyle}>
+          {label}
+        </span>
+        <div className="icon-group">
+          <button type="button" className="btn btn-outline-success btn-sm">
+            <Lightning className="icon" />
+          </button>
+
+          <button type="button" className="btn btn-outline-danger btn-sm">
+            <Trash className="icon" />
+          </button>
+        </div>
       </span>
-      <div className="icon-group">
-        <button type="button" className="btn btn-outline-success btn-sm">
-          <Lightning className="icon" />
-        </button>
-
-        <button type="button" className="btn btn-outline-danger btn-sm">
-          <Trash className="icon" />
-        </button>
-      </div>
-    </span>
-  );
+    );
+  }
 }
 
 export default TodoListItem;
